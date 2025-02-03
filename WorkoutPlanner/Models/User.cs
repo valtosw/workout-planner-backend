38e230e3ea0b4ce76112afd37344dd7e8474ce2d
Volodymyr Bessarabov <volodijbessarabov@knu.ua>
2025-02-03 23:47:55 +0200

    update user

--- WorkoutPlanner/Models/User.cs
+++ WorkoutPlanner/Models/User.cs
@@ -8,7 +8,7 @@ namespace WorkoutPlanner.Models
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string PasswordHash { get; set; }
+        public string Password { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string? ProfilePicture { get; set; }
