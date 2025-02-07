b95bbf7ef313b1d6e3559f9ac682297a6a014d0b
valtosw <volodijbessarabov@knu.ua>
2025-02-07 23:26:42 +0200

    add database context

---
+++ WorkoutPlanner/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WorkoutPlanner.Models;
+
+namespace WorkoutPlanner.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<TrainerRequest> TrainerRequests { get; set; }
+        public DbSet<WorkoutPlan> WorkoutPlans { get; set; }
+        public DbSet<ProgressLog> ProgressLogs { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<MuscleGroup> MuscleGroups { get; set; }
+        public DbSet<WorkoutPlanEntry> WorkoutPlanEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
