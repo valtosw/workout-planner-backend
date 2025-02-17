0ace0bd91f2c1343cc0fb27cf43602676449a38d
valtosw <volodijbessarabov@knu.ua>
2025-02-17 10:46:00 +0200

    add dataseeder

---
+++ WorkoutPlanner/Helpers/DataSeeder.cs
@@ -0,0 +1,32 @@
+﻿using WorkoutPlanner.Data;
+
+namespace WorkoutPlanner.Helpers
+{
+    public static class DataSeeder
+    {
+        public static void PopulateDb(IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            AddInitialData(serviceScope.ServiceProvider.GetService<AppDbContext>()!);
+        }
+
+        private static void AddInitialData(AppDbContext context)
+        {
+            if (!context.MuscleGroups.Any())
+            {
+                var muscleGroups = DatasetHandler.GetSeedData(Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "exercises_dataset.txt")).Item1;
+
+                context.MuscleGroups.AddRange(muscleGroups);
+                context.SaveChanges();
+            }
+
+            if (!context.Exercises.Any())
+            {
+                var exercises = DatasetHandler.GetSeedData(Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "exercises_dataset.txt")).Item2;
+
+                context.Exercises.AddRange(exercises);
+                context.SaveChanges();
+            }
+        }
+    }
+}
