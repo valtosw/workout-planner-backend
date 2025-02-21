804e370d925d78975262dba730309125f75f7895
valtosw <volodijbessarabov@knu.ua>
2025-02-21 15:21:31 +0200

    edit trainer model

--- WorkoutPlanner/Models/Trainer.cs
+++ WorkoutPlanner/Models/Trainer.cs
@@ -6,11 +6,13 @@ namespace WorkoutPlanner.Models
     {
         public string? Bio { get; set; }
         public string? Experience { get; set; }
+        public string? Location { get; set; }
         public string? PlaceOfWork { get; set; }
         public decimal TrainingPrice { get; set; }
         public string? InstagramLink { get; set; }
         public string? FacebookLink { get; set; }
         public string? TelegramLink { get; set; }
+        public bool IsPosted { get; set; }
 
         public ICollection<Customer> Customers { get; set; } = [];
         public ICollection<TrainerRequest> ReceivedRequests { get; set; } = [];
