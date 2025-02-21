151bc66e517c53a1286f1cd269e4245f53942018
valtosw <volodijbessarabov@knu.ua>
2025-02-21 15:21:43 +0200

    add trainer controller

---
+++ WorkoutPlanner/Controllers/TrainerController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WorkoutPlanner.Data;
+using WorkoutPlanner.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WorkoutPlanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainerController(AppDbContext context) : ControllerBase
+    {
+        // GET: api/<TrainerController>
+        [HttpGet]
+        public async Task<IEnumerable<Trainer>> GetAllTrainers()
+        {
+            var trainers = await context.Trainers.ToListAsync();
+
+            return trainers;
+        }
+
+        //// GET api/<TrainerController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<TrainerController>
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/<TrainerController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<TrainerController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
