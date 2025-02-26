e8f6edb9f1c08fc5888c5b1a79bc46344cb9e186
valtosw <volodijbessarabov@knu.ua>
2025-02-26 23:17:22 +0200

    Add AuthController with necessary namespaces and constructor

---
+++ WorkoutPlanner/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WorkoutPlanner.Data;
+using WorkoutPlanner.Models;
+
+namespace WorkoutPlanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(AppDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager) : ControllerBase
+    {
+        //[HttpPost]
+        //public async Task<>
+    }
+}
