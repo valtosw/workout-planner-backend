8a7d1129407fdf4b743463abff42531bc1ca1894
valtosw <volodijbessarabov@knu.ua>
2025-02-26 23:17:06 +0200

    Add LoginDto and RegisterDto classes

---
+++ WorkoutPlanner/Models/DTOs/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkoutPlanner.Models.DTOs
+{
+    public class LoginDto
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
---
+++ WorkoutPlanner/Models/DTOs/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkoutPlanner.Models.DTOs
+{
+    public class RegisterDto
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string Role { get; set; }
+    }
+}
