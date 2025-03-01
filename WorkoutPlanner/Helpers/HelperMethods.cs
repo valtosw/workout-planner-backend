826dfefaa0154665914f2d23884998d80eb73a64
valtosw <volodijbessarabov@knu.ua>
2025-03-01 22:36:27 +0200

    add a helper method to capitalize each word in a string

---
+++ WorkoutPlanner/Helpers/HelperMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace WorkoutPlanner.Helpers
+{
+    public class HelperMethods
+    {
+        public static string CapitalizeEachWord(string str)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
+        }
+    }
+}
