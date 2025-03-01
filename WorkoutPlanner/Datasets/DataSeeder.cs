1a654ee8211ce55a4c7b588a0705eddaddf38e63
valtosw <volodijbessarabov@knu.ua>
2025-03-01 22:36:05 +0200

    move dataset handling functionality to a new folder

---
+++ WorkoutPlanner/Datasets/DataSeeder.cs
@@ -0,0 +1,40 @@
+﻿using WorkoutPlanner.Data;
+
+namespace WorkoutPlanner.Datasets
+{
+    public static class DataSeeder
+    {
+        public static void PopulateDb(IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            AddInitialData(serviceScope.ServiceProvider.GetService<AppDbContext>()!);
+        }
+
+        private static void AddInitialData(AppDbContext context)
+        {
+            if (!context.MuscleGroups.Any())
+            {
+                var muscleGroups = DatasetHandler.GetMuscleGroupAndExerciseData(Path.Combine(Directory.GetCurrentDirectory(), "Datasets", "DatasetFiles", "exercises_dataset.txt")).Item1;
+
+                context.MuscleGroups.AddRange(muscleGroups);
+                context.SaveChanges();
+            }
+
+            if (!context.Exercises.Any())
+            {
+                var exercises = DatasetHandler.GetMuscleGroupAndExerciseData(Path.Combine(Directory.GetCurrentDirectory(), "Datasets", "DatasetFiles", "exercises_dataset.txt")).Item2;
+
+                context.Exercises.AddRange(exercises);
+                context.SaveChanges();
+            }
+
+            if (!context.Countries.Any())
+            {
+                var countries = DatasetHandler.GetCountryData(Path.Combine(Directory.GetCurrentDirectory(), "Datasets", "DatasetFiles", "countries_dataset.txt"));
+
+                context.Countries.AddRange(countries);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ WorkoutPlanner/Datasets/DatasetHandler.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.RegularExpressions;
+using WorkoutPlanner.Helpers;
+using WorkoutPlanner.Models;
+
+namespace WorkoutPlanner.Datasets
+{
+    public class DatasetHandler
+    {
+        public static (List<MuscleGroup>, List<Exercise>) GetMuscleGroupAndExerciseData(string filePath)
+        {
+
+
+            var muscleGroupsDict = new Dictionary<string, MuscleGroup>();
+            var exercises = new List<Exercise>();
+
+            var lines = File.ReadAllLines(filePath);
+
+            int muscleGroupIdCounter = 1;
+            int exerciseIdCounter = 1;
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(',');
+
+                if (parts.Length != 2)
+                    continue;
+
+                string exerciseName = parts[0].Trim();
+                string muscleGroupName = parts[1].Trim();
+
+                if (!muscleGroupsDict.TryGetValue(muscleGroupName, out var muscleGroup))
+                {
+                    muscleGroup = new MuscleGroup
+                    {
+                        Id = muscleGroupIdCounter++,
+                        Name = HelperMethods.CapitalizeEachWord(muscleGroupName),
+                        Exercises = []
+                    };
+                    muscleGroupsDict[muscleGroupName] = muscleGroup;
+                }
+
+                var exercise = new Exercise
+                {
+                    Id = exerciseIdCounter++,
+                    Name = HelperMethods.CapitalizeEachWord(exerciseName),
+                    MuscleGroupId = muscleGroup.Id,
+                    MuscleGroup = muscleGroup
+                };
+
+                muscleGroup.Exercises.Add(exercise);
+                exercises.Add(exercise);
+            }
+
+            return (muscleGroupsDict.Values.ToList(), exercises);
+        }
+
+        public static List<Country> GetCountryData(string filePath)
+        {
+            var countries = new List<Country>();
+            var lines = File.ReadAllLines(filePath);
+            var regex = new Regex(@"^(.*?)\s\([A-Z]{2}\)$"); 
+
+            int id = 1;
+            foreach (var line in lines)
+            {
+                var match = regex.Match(line);
+                if (match.Success)
+                {
+                    countries.Add(new Country { Id = id++, Name = match.Groups[1].Value });
+                }
+            }
+
+            return countries;
+        }
+    }
+}
