b43b609bae57283320b054170564356e89b8dda6
valtosw <volodijbessarabov@knu.ua>
2025-03-01 22:37:01 +0200

    remove dtos

--- WorkoutPlanner/Models/DTOs/LoginDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WorkoutPlanner.Models.DTOs
-{
-    public class LoginDto
-    {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-    }
-}
