392055eb9492e3d8d449ab7a1a090b7c979fc548
valtosw <volodijbessarabov@knu.ua>
2025-03-01 22:38:09 +0200

    add MuscleGroupController for retrieving muscle groups

---
+++ WorkoutPlanner/Controllers/MuscleGroupController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkoutPlanner.Data;
+
+namespace WorkoutPlanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MuscleGroupController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet("AllMuscleGroups")]
+        public async Task<IEnumerable<string>> GetAllMuscleGroups()
+        {
+            return await context.MuscleGroups.Select(mg => mg.Name).ToListAsync();
+        }
+    }
+}
