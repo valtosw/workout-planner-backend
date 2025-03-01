6c6338adf5cd59b26f0d40a1a43d14f57f05e66c
valtosw <volodijbessarabov@knu.ua>
2025-03-01 22:37:56 +0200

    add ExerciseController for muscle group exercises

---
+++ WorkoutPlanner/Controllers/ExerciseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkoutPlanner.Data;
+
+namespace WorkoutPlanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExerciseController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet("ExercisesByMuscleGroup")]
+        public async Task<IEnumerable<string>> GetExercisesByMuscleGroup(string muscleGroupName)
+        {
+            var muscleGroup = await context.MuscleGroups
+                .Include(mg => mg.Exercises)
+                .Where(mg => mg.Name == muscleGroupName)
+                .FirstOrDefaultAsync();
+
+            if (muscleGroup is null)
+            {
+                return [];
+            }
+            return muscleGroup.Exercises.Select(e => e.Name);
+        }
+    }
+}
