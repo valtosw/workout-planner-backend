1e4947d8e248ff06918f04c1516c1043cbaa6152
valtosw <volodijbessarabov@knu.ua>
2025-03-05 23:10:29 +0200

    Add JwtSettings class for JWT configuration

---
+++ WorkoutPlanner/Models/AuthModels/JwtSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace WorkoutPlanner.Models.AuthModels
+{
+    public class JwtSettings
+    {
+        public string AccessTokenSecret { get; set; }
+        public string RefreshTokenSecret { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int AccessTokenExpiryInSeconds { get; set; }
+        public int RefreshTokenExpiryInSeconds { get; set; }
+    }
+}
