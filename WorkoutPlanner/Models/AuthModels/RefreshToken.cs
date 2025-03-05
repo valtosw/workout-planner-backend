ce303ccdd7d89aa2cec9229abfb1e27a6734a0c5
valtosw <volodijbessarabov@knu.ua>
2025-03-05 23:10:17 +0200

    Add RefreshToken class to AuthModels namespace

---
+++ WorkoutPlanner/Models/AuthModels/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkoutPlanner.Models.AuthModels
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Token { get; set; }
+
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
