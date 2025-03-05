4bcbe279ba3ae90985f9ddaa2da3164b5d828d9a
valtosw <volodijbessarabov@knu.ua>
2025-03-05 23:11:12 +0200

    Add ApplicationUserController for user management

---
+++ WorkoutPlanner/Controllers/ApplicationUserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorkoutPlanner.Data;
+using WorkoutPlanner.Models;
+
+namespace WorkoutPlanner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApplicationUser>> GetUserById(string id)
+        {
+            var user = await context.Users
+                .FirstOrDefaultAsync(u => u.Id == id);
+            return user is null ? NotFound() : Ok(user);
+        }
+    }
+}
