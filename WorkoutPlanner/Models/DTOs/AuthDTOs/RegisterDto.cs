8021e798d6b8997276f02b5dd00cb0b0612de1e9
valtosw <volodijbessarabov@knu.ua>
2025-03-05 23:10:07 +0200

    Add LoginDto and RegisterDto classes for authentication

---
+++ WorkoutPlanner/Models/DTOs/AuthDTOs/RegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorkoutPlanner.Models.DTOs.AuthDTOs
+{
+    public class RegisterDto
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string Role { get; set; }
+    }
+}
