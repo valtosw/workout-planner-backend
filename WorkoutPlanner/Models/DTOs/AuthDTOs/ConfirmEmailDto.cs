6e85f944d861889f63d47c86bf262e6cdddadc94
valtosw <volodijbessarabov@knu.ua>
2025-03-09 22:17:59 +0200

    Add ConfirmEmailDto class for email confirmation

---
+++ WorkoutPlanner/Models/DTOs/AuthDTOs/ConfirmEmailDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WorkoutPlanner.Models.DTOs.AuthDTOs
+{
+    public class ConfirmEmailDto
+    {
+        public string Email { get; set; }
+        public string Code { get; set; }
+    }
+}
