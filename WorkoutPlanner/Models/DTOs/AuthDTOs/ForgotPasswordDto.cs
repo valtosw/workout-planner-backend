d6a1be8ccc3d73e4f7e242ec1937ac352c9be423
valtosw <volodijbessarabov@knu.ua>
2025-03-14 21:01:36 +0200

    Add DTOs for password reset functionality

---
+++ WorkoutPlanner/Models/DTOs/AuthDTOs/ForgotPasswordDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WorkoutPlanner.Models.DTOs.AuthDTOs
+{
+    public class ForgotPasswordDto
+    {
+        public string Email { get; set; }
+    }
+}
---
+++ WorkoutPlanner/Models/DTOs/AuthDTOs/ResetPasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WorkoutPlanner.Models.DTOs.AuthDTOs
+{
+    public class ResetPasswordDto
+    {
+        public string Email { get; set; }
+        public string Code { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
