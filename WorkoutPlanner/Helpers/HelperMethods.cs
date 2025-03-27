ebf43b0fd5721c7ac12f71f8aade8384ac74fe14
valtosw <volodijbessarabov@knu.ua>
2025-03-27 22:30:49 +0200

    Refactor HelperMethods to static class with Shuffle method

--- WorkoutPlanner/Helpers/HelperMethods.cs
+++ WorkoutPlanner/Helpers/HelperMethods.cs
@@ -2,11 +2,24 @@
 
 namespace WorkoutPlanner.Helpers
 {
-    public class HelperMethods
+    public static class HelperMethods
     {
         public static string CapitalizeEachWord(string str)
         {
             return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
+        }
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            Random rng = new();
+
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                (list[n], list[k]) = (list[k], list[n]);
+            }
         }
     }
 }
