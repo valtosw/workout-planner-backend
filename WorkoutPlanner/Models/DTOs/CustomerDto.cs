6b5f675ac1bfc94a52c92935a65cdb3b590e3270
valtosw <volodijbessarabov@knu.ua>
2025-03-31 11:45:01 +0300

    Add CustomerDto class to DTOs namespace

---
+++ WorkoutPlanner/Models/DTOs/CustomerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkoutPlanner.Models.DTOs
+{
+    public class CustomerDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
