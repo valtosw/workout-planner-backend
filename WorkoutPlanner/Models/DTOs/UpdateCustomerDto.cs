48bcfb30140425e60f957759f503b2e63f4e87f5
valtosw <volodijbessarabov@knu.ua>
2025-04-06 00:24:52 +0300

    add updatecustomerdto

---
+++ WorkoutPlanner/Models/DTOs/UpdateCustomerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkoutPlanner.Models.DTOs
+{
+    public class UpdateCustomerDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public IFormFile? ProfilePicture { get; set; }
+    }
+}
