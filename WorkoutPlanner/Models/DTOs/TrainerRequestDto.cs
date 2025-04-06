57ccd96513604a7d32409b0af889ac88083cc42d
valtosw <volodijbessarabov@knu.ua>
2025-04-06 23:26:36 +0300

    Add TrainerRequestDto class to DTOs namespace

---
+++ WorkoutPlanner/Models/DTOs/TrainerRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkoutPlanner.Models.DTOs
+{
+    public class TrainerRequestDto
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public string? UserProfilePicture { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+    }
+}
