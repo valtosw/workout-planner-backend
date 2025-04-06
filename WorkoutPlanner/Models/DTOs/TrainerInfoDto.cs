c3ac77e9766675fe3ea813e394bafec7373273ad
valtosw <volodijbessarabov@knu.ua>
2025-04-06 20:32:02 +0300

    add new dtos

---
+++ WorkoutPlanner/Models/DTOs/TrainerInfoDto.cs
@@ -0,0 +1,18 @@
+﻿namespace WorkoutPlanner.Models.DTOs
+{
+    public class TrainerInfoDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? ProfilePicture { get; set; }
+        public int Experience { get; set; }
+        public string? Country { get; set; }
+        public string? City { get; set; }
+        public string? PlaceOfWork { get; set; }
+        public decimal TrainingPrice { get; set; }
+        public string? Bio { get; set; }
+        public string? InstagramLink { get; set; }
+        public string? FacebookLink { get; set; }
+        public string? TelegramLink { get; set; }
+    }
+}
