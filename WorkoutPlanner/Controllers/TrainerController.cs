764ab6539807bdb5a655438b530c54715b59d693
valtosw <volodijbessarabov@knu.ua>
2025-04-06 20:32:12 +0300

    add update trainer endpoint

--- WorkoutPlanner/Controllers/TrainerController.cs
+++ WorkoutPlanner/Controllers/TrainerController.cs
@@ -15,6 +15,96 @@ namespace WorkoutPlanner.Controllers
     [ApiController]
     public class TrainerController(AppDbContext context) : ControllerBase
     {
+        [HttpGet("{id}")]
+        public async Task<TrainerInfoDto> GetTrainerInfoById(string id)
+        {
+            var trainer = await context.Trainers
+                .Where(t => t.Id == id)
+                .Include(t => t.Country)
+                .Select(t => new TrainerInfoDto
+                {
+                    FirstName = t.FirstName,
+                    LastName = t.LastName,
+                    ProfilePicture = t.ProfilePicture,
+                    Experience = t.Experience,
+                    Country = t.Country!.Name,
+                    City = t.City,
+                    PlaceOfWork = t.PlaceOfWork,
+                    TrainingPrice = t.TrainingPrice,
+                    Bio = t.Bio,
+                    InstagramLink = t.InstagramLink,
+                    FacebookLink = t.FacebookLink,
+                    TelegramLink = t.TelegramLink
+                }).FirstOrDefaultAsync();
+            return trainer ?? new TrainerInfoDto();
+        }
+
+        [HttpPost("UpdateTrainerProfile")]
+        public async Task UpdateTrainerProfile([FromForm] UpdateTrainerDto updatedTrainer)
+        {
+            var trainer = await context.Trainers.FindAsync(updatedTrainer.Id);
+
+            if (!string.IsNullOrEmpty(updatedTrainer.FirstName))
+            {
+                trainer!.FirstName = updatedTrainer.FirstName;
+            }
+            if (!string.IsNullOrEmpty(updatedTrainer.LastName))
+            {
+                trainer!.LastName = updatedTrainer.LastName;
+            }
+            if (updatedTrainer.ProfilePicture is not null)
+            {
+                using var ms = new MemoryStream();
+                await updatedTrainer.ProfilePicture.CopyToAsync(ms);
+                var imageBytes = ms.ToArray();
+                trainer!.ProfilePicture = $"data:image/png;base64,{Convert.ToBase64String(imageBytes)}";
+            }
+
+            trainer!.Experience = updatedTrainer.Experience;
+
+            if (!string.IsNullOrEmpty(updatedTrainer.Country))
+            {
+                var country = await context.Countries
+                    .FirstOrDefaultAsync(c => c.Name == updatedTrainer.Country);
+
+                if (country is not null) { trainer!.CountryId = country.Id; }
+            }
+
+            if (!string.IsNullOrEmpty(updatedTrainer.City))
+            {
+                trainer!.City = updatedTrainer.City;
+            }
+
+            if (!string.IsNullOrEmpty(updatedTrainer.PlaceOfWork))
+            {
+                trainer!.PlaceOfWork = updatedTrainer.PlaceOfWork;
+            }
+
+            trainer!.TrainingPrice = updatedTrainer.TrainingPrice;
+
+            if (!string.IsNullOrEmpty(updatedTrainer.Bio))
+            {
+                trainer!.Bio = updatedTrainer.Bio;
+            }
+
+            if (!string.IsNullOrEmpty(updatedTrainer.InstagramLink))
+            {
+                trainer!.InstagramLink = updatedTrainer.InstagramLink;
+            }
+
+            if (!string.IsNullOrEmpty(updatedTrainer.FacebookLink))
+            {
+                trainer!.FacebookLink = updatedTrainer.FacebookLink;
+            }
+
+            if (!string.IsNullOrEmpty(updatedTrainer.TelegramLink))
+            {
+                trainer!.TelegramLink = updatedTrainer.TelegramLink;
+            }
+
+            await context.SaveChangesAsync();
+        }
+
         [HttpGet("Posted")]
         public async Task<IEnumerable<Trainer>> GetAllPostedTrainers()
         {
