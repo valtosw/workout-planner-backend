3e1214aa6976c906fd6dcc38db1a9007a0d2578d
valtosw <volodijbessarabov@knu.ua>
2025-04-06 23:25:44 +0300

    migrations stuff

---
+++ WorkoutPlanner/Migrations/20250406173608_UpdateModelMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkoutPlanner.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateModelMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RequestStatus",
+                table: "TrainerRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequestStatus",
+                table: "TrainerRequests");
+        }
+    }
+}
