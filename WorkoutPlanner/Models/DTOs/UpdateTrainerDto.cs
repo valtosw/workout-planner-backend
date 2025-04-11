92ebeea36e647855e29760c021d8bd35584007fd
valtosw <volodijbessarabov@knu.ua>
2025-04-11 12:57:56 +0300

    update dto

--- WorkoutPlanner/Models/DTOs/UpdateTrainerDto.cs
+++ WorkoutPlanner/Models/DTOs/UpdateTrainerDto.cs
@@ -3,17 +3,17 @@
     public class UpdateTrainerDto
     {
         public string Id { get; set; }
-        public string FirstName { get; set; } = "";
-        public string LastName { get; set; } = "";
-        public string Bio { get; set; } = "";
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Bio { get; set; }
         public int Experience { get; set; }
-        public string City { get; set; } = "";
-        public string Country { get; set; } = "";
-        public string PlaceOfWork { get; set; } = "";
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public string? PlaceOfWork { get; set; }
         public decimal TrainingPrice { get; set; }
-        public string InstagramLink { get; set; } = "";
-        public string FacebookLink { get; set; } = "";
-        public string TelegramLink { get; set; } = "";
+        public string? InstagramLink { get; set; }
+        public string? FacebookLink { get; set; }
+        public string? TelegramLink { get; set; }
         public IFormFile? ProfilePicture { get; set; }
     }
 }
