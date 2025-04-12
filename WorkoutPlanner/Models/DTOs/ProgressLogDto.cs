1d14c32c44f170cab8faa4abc6e2befd1a6b73c5
valtosw <volodijbessarabov@knu.ua>
2025-04-12 17:16:52 +0300

    Add ProgressLogDto class for exercise logging

---
+++ WorkoutPlanner/Models/DTOs/ProgressLogDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkoutPlanner.Models.DTOs
+{
+    public class ProgressLogDto
+    {
+        public required string MuscleGroupName { get; set; }
+        public required string ExerciseName { get; set; }
+        public required DateTime LogDate { get; set; }
+        public required float Weight { get; set; }
+    }
+}
