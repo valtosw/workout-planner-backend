b5b129b0e5d269dbba2d0edac7d6270b68dc45b7
valtosw <volodijbessarabov@knu.ua>
2025-04-12 23:27:48 +0300

    Remove MuscleGroupName from ProgressLogDto

--- WorkoutPlanner/Models/DTOs/ProgressLogDto.cs
+++ WorkoutPlanner/Models/DTOs/ProgressLogDto.cs
@@ -2,7 +2,6 @@
 {
     public class ProgressLogDto
     {
-        public required string MuscleGroupName { get; set; }
         public required string ExerciseName { get; set; }
         public required DateTime LogDate { get; set; }
         public required float Weight { get; set; }
