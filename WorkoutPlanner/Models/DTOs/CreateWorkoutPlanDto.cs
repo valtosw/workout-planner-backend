525fb90d9d9d20054dc2e0603c39f8290ebaf913
valtosw <volodijbessarabov@knu.ua>
2025-04-13 19:50:48 +0300

    Add CreateWorkoutPlanDto class for workout plans

---
+++ WorkoutPlanner/Models/DTOs/CreateWorkoutPlanDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkoutPlanner.Models.DTOs
+{
+    public class CreateWorkoutPlanDto
+    {
+        public string Name { get; set; }
+        public string CreatedById { get; set; }
+        public string? AssignedToId { get; set; }
+        public IEnumerable<EntryToAddDto> WorkoutPlanEntries { get; set; }
+    }
+}
