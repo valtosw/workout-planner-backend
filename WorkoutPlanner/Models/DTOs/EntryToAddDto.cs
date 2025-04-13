bea54aed955d892b0655607b31954f53f1567db2
valtosw <volodijbessarabov@knu.ua>
2025-04-13 19:50:35 +0300

    Add EntryToAddDto class for exercise entry details

---
+++ WorkoutPlanner/Models/DTOs/EntryToAddDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkoutPlanner.Models.DTOs
+{
+    public class EntryToAddDto
+    {
+        public int Reps { get; set; }
+        public int Sets { get; set; }
+        public float Weight { get; set; }
+        public string Exercise { get; set; }
+    }
+}
